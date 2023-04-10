d7f6b386103db6b822ee115c2b0228c27fd92199
imLIVI <lesly120898@gmail.com>
2023-04-11 00:31:59 +0300

    Add a class to parse the conditions that are after WHERE

---
+++ src/com/digdes/school/Condition.java
@@ -0,0 +1,41 @@
+package com.digdes.school;
+
+public class Condition {
+    private String field;
+    private String sign;
+    private Object value;
+    private String nextOperator;
+
+    public String getNextOperator() {
+        return nextOperator;
+    }
+
+    public void setNextOperator(String nextOperator) {
+        this.nextOperator = nextOperator;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+}
